99174c8358967257f51b4db05ab2295595d06232
rodrigociro <rodrigociro@hotmail.es>
2023-08-04 19:24:15 +0000

    test

--- database/create_table.py
+++ database/create_table.py
@@ -2,6 +2,6 @@ import sqlite3
 
 conexion = sqlite3.connect("basededatos1.db")
 cursor = conexion.cursor()
-cursor.execute("CREATE TABLE PERSONAS(nombre TEXT, apellido1 TEXT, apellido2 TEXT, edad INTEGER)")
+cursor.execute("CREATE TABLE PERSONAS (nombre TEXT, apellido1 TEXT, apellido2 TEXT, edad INTEGER)")
 conexion.commit()
 conexion.close()
